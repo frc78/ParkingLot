dc3bf2c38380afb41485b2cda773d877527ab9cb
JR <jr@aquidneckislandrobotics.org>
2022-02-20 17:01:55 -0500

    Created constants, fixed fall command, adjustments
    # Conflicts:
    #	src/main/java/frc/robot/Constants.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -102,7 +102,6 @@ public class RobotContainer {
     m_shooter.setDefaultCommand(new PerpetualCommand(new InstantCommand(m_shooter::stopWheely, m_shooter)));
     m_feed.setDefaultCommand(new PerpetualCommand(new InstantCommand(m_feed::stopFeed, m_feed)));
     m_feedWheel.setDefaultCommand(new PerpetualCommand(new InstantCommand(m_feedWheel::stopFeedWheel, m_feedWheel)));
-
     m_hanger.setDefaultCommand(new DeployHanger(m_hanger, m_manipController));
   }
 
